d76c8b67ba7c020217eb49e252b4aa4811ee38bb
Paco Gomez <contact@pacogomez.com>
2014-11-20 23:05:59 -0600

    setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# VMware vCloud Air Python SDK
+# VMware vCloud Python SDK
 # Copyright (c) 2014 VMware, Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");

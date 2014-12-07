f243c313bf60b79c2b03baed3887c3c254bf1160
Paco Gomez <contact@pacogomez.com>
2014-12-06 23:02:28 -0600

    v0.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pyvcloud',
-    version='0.1',
+    version='0.2',
     description='VMware vCloud Python SDK',
     long_description=read('README.rst'),
     url='https://github.com/vmware/pyvcloud',

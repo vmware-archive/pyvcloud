baa23ff82cfb70e3f6455ab5392162917fb7947a
Oleksandr Chyrko <aleksandr.chirko@gmail.com>
2014-12-22 18:19:05 +0200

    Add connect/disconnect vm and network

--- pyvcloud/vapp.py
+++ pyvcloud/vapp.py
@@ -164,7 +164,10 @@ class VAPP(object):
 
     def delete_snapshot(self, args):
         self.execute("snapshot:removeAll", args["--blocking"], "can't have its snapshot deleted", "post", args["--json"])
-        
+
+    def customize_on_next_poweron(self):
+        self.execute("customizeAtNextPowerOn", True, "Can't customize", "post", True)
+
     # create instantiateVAppTemplateParams python object (read "vCloud API Programming Guide", pages 32 and 33)
     @staticmethod
     def create_instantiateVAppTemplateParams(name, template_href, deploy="true", power="true"):
@@ -322,3 +325,135 @@ class VAPP(object):
                 return self.execute("disk:detach", True, "can't be detached", "post", True, body=body, targetVM=vms[0])        
         
                         
+    def _modify_networkConnectionSection(self, section, new_connection,
+                                         primary_index=None):
+
+        for networkConnection in section.get_NetworkConnection():
+            if (networkConnection.get_network().lower() ==
+                new_connection.get_network().lower()):
+                return (False,
+                        "VApp {0} is already connected to org vdc network {1}"
+                        .format(self.name, networkConnection.get_network()))
+
+        section.add_NetworkConnection(new_connection)
+        if section.get_Info() is None:
+            info = vcloudType.Msg_Type()
+            info.set_valueOf_("Network connection")
+            section.set_Info(info)
+        if primary_index is not None:
+            section.set_PrimaryNetworkConnectionIndex(primary_index)
+
+    def _create_networkConnection(self, network_name, index, ip_allocation_mode,
+                                 mac_address=None, ip_address=None):
+        networkConnection = vcloudType.NetworkConnectionType()
+        networkConnection.set_network(network_name)
+        networkConnection.set_NetworkConnectionIndex(index)
+        networkConnection.set_IpAddressAllocationMode(ip_allocation_mode)
+        networkConnection.set_IsConnected(True)
+        if ip_address and ip_allocation_mode == 'MANUAL':
+            networkConnection.set_IpAddress(ip_address)
+        if mac_address:
+            networkConnection.set_MACAddress(mac_address)
+        return networkConnection
+
+    def connect_network(self, network_name, connection_index,
+                        connections_primary_index=None,
+                        ip_allocation_mode='DHCP', mac_address=None,
+                        ip_address=None):
+        section, link = self._get_network_connection_data()
+        new_connection = self._create_networkConnection(
+            network_name, connection_index, ip_allocation_mode, mac_address,
+            ip_address)
+        self._modify_networkConnectionSection(section,
+                                              new_connection,
+                                              connections_primary_index)
+
+        body = self._create_request_body(
+            section,
+            'NetworkConnectionSection',
+            'xmlns="http://www.vmware.com/vcloud/v1.5" '
+            'xmlns:ovf="http://schemas.dmtf.org/ovf/envelope/1"')
+
+        response = requests.put(link.get_href(),
+                                data = body,
+                                headers = self.headers)
+        if response.status_code == requests.codes.accepted:
+            task = taskType.parseString(response.content, True)
+            return True, task
+        else:
+            return False, response.content
+
+    def disconnect_network(self, network_name):
+        section, link = self._get_network_connection_data()
+
+        found = None
+        for index, nwConnection in enumerate(section.get_NetworkConnection()):
+            if nwConnection.get_network() == network_name:
+                found = index
+
+        if found is None:
+            return False, "Network {0} could not be found".format(network_name)
+        else:
+            section.NetworkConnection.pop(found)
+
+            body = self._create_request_body(
+                section,
+                'NetworkConnectionSection',
+                'xmlns="http://www.vmware.com/vcloud/v1.5" '
+                'xmlns:ovf="http://schemas.dmtf.org/ovf/envelope/1"')
+
+            response = requests.put(link.get_href(),
+                                    data = body,
+                                    headers = self.headers)
+            if response.status_code == requests.codes.accepted:
+                task = taskType.parseString(response.content, True)
+                return True, task
+            else:
+                return False, response.content
+
+    def update_guest_customization(self, customization_script=None):
+        vm = self._get_vms()[0]
+        customization_section = [section for section in vm.get_Section()
+                                 if (section.__class__.__name__ ==
+                                     "GuestCustomizationSectionType")
+                                 ][0]
+        if customization_script:
+            customization_section.set_CustomizationScript(customization_script)
+        body = self._create_request_body(
+            customization_section,
+            'GuestCustomizationSectionType',
+            'xmlns="http://www.vmware.com/vcloud/v1.5" '
+            'xmlns:ovf="http://schemas.dmtf.org/ovf/envelope/1"')
+
+        response = requests.put(customization_section.get_href(),
+                                data = body,
+                                headers = self.headers)
+        if response.status_code == requests.codes.accepted:
+            task = taskType.parseString(response.content, True)
+            return True, task
+        else:
+            return False, response.content
+
+    def _create_request_body(self, obj, name, namespacedef):
+        body = ghf.convertPythonObjToStr(obj,
+                                         name=name,
+                                         namespacedef=namespacedef)
+        body = body.replace("vmw:", "")
+        body = body.replace('Info msgid=""', "ovf:Info")
+        body = body.replace('Info xmlns:vmw="http://www.vmware.com/vcloud/'
+                            'v1.5" msgid=""',
+                            "ovf:Info")
+        body = body.replace("/Info", "/ovf:Info")
+        return body
+
+    def _get_network_connection_data(self):
+        vm = self._get_vms()[0]
+        vm_nw_conn_section = [section for section in vm.get_Section()
+                                if (section.__class__.__name__ ==
+                                    "NetworkConnectionSectionType")
+                                     ][0]
+        link = [link for link in vm_nw_conn_section.get_Link()
+                if (link.get_type() ==
+                    "application/vnd.vmware.vcloud.networkConnectionSection+xml")
+                ][0]
+        return vm_nw_conn_section, link

5c4957f357972774faadbab5370af34817402e0c
Paco Gomez <contact@pacogomez.com>
2015-01-12 09:32:00 -0600

    v0.5

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='vca-cli',
-    version='0.4',
+    version='0.5',
     description='VMware vCloud CLI',
     url='https://github.com/vmware/vca-cli',
     author='VMware, Inc.',
@@ -26,7 +26,7 @@ setup(
         'Click',
         # Colorama is only required for Windows.
         'colorama',
-        'pyvcloud == 0.4'
+        'pyvcloud == 0.5'
     ],
     license='License :: OSI Approved :: Apache Software License',
     classifiers=[

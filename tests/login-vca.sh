0cdc04edfe5cc01e7ca65e10abd0eceb1f252c80
Paco Gomez <contact@pacogomez.com>
2015-07-23 17:07:19 -0500

    misc fixes

--- tests/login-vca.sh
+++ tests/login-vca.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-vca login $VCA_CLI_USER --password $VCA_CLI_PASSWORD --instance $VCA_CLI_INSTANCE
+vca login $VCA_CLI_USER --password $VCA_CLI_PASS --instance $VCA_CLI_INSTANCE
 vca status

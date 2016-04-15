6fdd39ad65742de48891cefc58c693707b8345db
Paco Gomez <contact@pacogomez.com>
2016-04-14 22:48:02 -0500

    v16

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ requirements = [
 
 setup(
     name='vca-cli',
-    version='15',
+    version='16',
     description='VMware vCloud CLI',
     url='https://github.com/vmware/vca-cli',
     author='VMware, Inc.',

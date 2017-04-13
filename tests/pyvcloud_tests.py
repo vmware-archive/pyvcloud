af226ceb55d11115b467763f5c4b4d5850931969
Paco Gómez <contact@pacogomez.com>
2017-04-13 12:50:52 -0500

    travis tests

--- tests/pyvcloud_tests.py
+++ tests/pyvcloud_tests.py
@@ -4,5 +4,4 @@ import pkg_resources
 
 def test_0001():
     """Check version"""
-    # print('version=%s' % pkg_resources.require("pyvcloud")[0].version)
     assert '16' == pkg_resources.require("pyvcloud")[0].version

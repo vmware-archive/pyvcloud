29e7d8435f111d62a9968c5c4e53b16555be3b74
Paco Gómez <contact@pacogomez.com>
2017-04-14 10:56:12 -0500

    vcd 8.20

--- vca_cli/vca_cli.py
+++ vca_cli/vca_cli.py
@@ -125,8 +125,8 @@ def profile(cmd_proc):
 @click.option('-d', '--do-not-save-password', is_flag=True,
               default=False, help='Do not save password')
 @click.option('-v', '--version', 'service_version',
-              default='5.7', metavar='[5.1 | 5.5 | 5.6 | 5.7]',
-              type=click.Choice(['5.1', '5.5', '5.6', '5.7']), help='')
+              default='5.7', metavar='[5.1 | 5.5 | 5.6 | 5.7 | 27.0]',
+              type=click.Choice(['5.1', '5.5', '5.6', '5.7', '27.0']), help='')
 @click.option('-H', '--host', default='https://vca.vmware.com',
               metavar='<host>',
               help='')

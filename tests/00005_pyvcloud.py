7eb5668de38b2d929cbb130bfd85eb913e3cb383
Paco Gómez <contact@pacogomez.com>
2017-07-18 15:37:13 -0500

    pbr

--- tests/00005_pyvcloud.py
+++
@@ -1,7 +0,0 @@
-from nose.tools import with_setup
-from testconfig import config
-import pkg_resources
-
-def test_0001():
-    """Check version"""
-    assert '18.0.3' == pkg_resources.require("pyvcloud")[0].version

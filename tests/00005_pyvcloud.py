a1d7b2d69c2ee6de443d18af8fcd40ce2ec050b5
Paco Gómez <contact@pacogomez.com>
2017-07-18 13:01:09 -0500

    18.0.3

--- tests/00005_pyvcloud.py
+++ tests/00005_pyvcloud.py
@@ -4,4 +4,4 @@ import pkg_resources
 
 def test_0001():
     """Check version"""
-    assert '18.0.2' == pkg_resources.require("pyvcloud")[0].version
+    assert '18.0.3' == pkg_resources.require("pyvcloud")[0].version

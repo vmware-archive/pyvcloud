aa7d802e49d524b7be22de5510380e4ab653fc60
Paco Gómez <contact@pacogomez.com>
2017-08-17 23:45:46 -0500

    catalog items

--- vcd_cli/catalog.py
+++ vcd_cli/catalog.py
@@ -14,6 +14,7 @@
 import click
 from pyvcloud.vcd.org import Org
 from pyvcloud.vcd.utils import task_to_dict
+from pyvcloud.vcd.utils import to_dict
 from vcd_cli.utils import restore_session
 from vcd_cli.utils import stderr
 from vcd_cli.utils import stdout
@@ -38,35 +39,34 @@ def catalog(ctx):
 \b
         vcd catalog info 'my catalog'
             Get details of a catalog.
+\b
+        vcd catalog list-items 'my catalog'
+            Get list of items in a catalog.
     """  # NOQA
-    if ctx.invoked_subcommand is not None:
+    if ctx.invoked_subcommand not in [None, 'item']:
         try:
             restore_session(ctx)
         except Exception as e:
             stderr(e, ctx)
 
 
-@catalog.command(short_help='show catalog details')
+@catalog.command(short_help='show catalog or item details')
 @click.pass_context
-@click.argument('name',
-                metavar='<name>',
+@click.argument('catalog-name',
+                metavar='<catalog-name>',
                 required=True)
-def info(ctx, name):
+@click.argument('item-name',
+                metavar='[item-name]',
+                required=False,
+                default=None)
+def info(ctx, catalog_name, item_name):
     try:
         client = ctx.obj['client']
         org_name = ctx.obj['profiles'].get('org')
-        in_use_org_name = ctx.obj['profiles'].get('org_in_use')
         in_use_org_href = ctx.obj['profiles'].get('org_href')
         org = Org(client, in_use_org_href, org_name == 'System')
-        result = None
-        catalogs = org.list_catalogs()
-        for c in catalogs:
-            if c['name'] == name and c['orgName'] == in_use_org_name:
-                result = c
-                break
-        if result is None:
-            raise Exception('Catalog not found.')
-        stdout(result, ctx)
+        result = org.get_catalog(catalog_name)
+        stdout(to_dict(result), ctx)
     except Exception as e:
         stderr(e, ctx)
 
@@ -87,19 +87,19 @@ def list(ctx):
 
 @catalog.command(short_help='create a catalog')
 @click.pass_context
-@click.argument('name',
+@click.argument('catalog-name',
                 metavar='<catalog-name>')
 @click.option('-d',
               '--description',
               required=False,
               default='',
               metavar='[description]')
-def create(ctx, name, description):
+def create(ctx, catalog_name, description):
     try:
         client = ctx.obj['client']
         in_use_org_href = ctx.obj['profiles'].get('org_href')
         org = Org(client, in_use_org_href)
-        result = org.create_catalog(name, description)
+        result = org.create_catalog(catalog_name, description)
         stdout(task_to_dict(result), ctx, show_id=True)
     except Exception as e:
         stderr(e, ctx)
@@ -107,14 +107,97 @@ def create(ctx, name, description):
 
 @catalog.command(short_help='delete a catalog')
 @click.pass_context
-@click.argument('name',
+@click.argument('catalog-name',
                 metavar='<catalog-name>')
-def delete(ctx, name):
+def delete(ctx, catalog_name):
     try:
         client = ctx.obj['client']
         in_use_org_href = ctx.obj['profiles'].get('org_href')
         org = Org(client, in_use_org_href)
-        org.delete_catalog(name)
+        org.delete_catalog(catalog_name)
         stdout('Catalog deleted.', ctx)
+    except Exception as e:
+        stderr(e, ctx)
+
+
+def callback(transferred, total):
+    print('transferred %s of %s bytes' % (transferred, total))
+
+
+@catalog.command(short_help='upload file to catalog')
+@click.pass_context
+@click.argument('catalog-name',
+                metavar='<catalog-name>')
+@click.argument('file_name',
+                type=click.Path(exists=True),
+                metavar='<file-name>',
+                required=True)
+@click.option('-i',
+              '--item-name',
+              required=False,
+              default=None,
+              metavar='[item-name]')
+@click.option('-p/-n',
+              '--progress/--no-progress',
+              is_flag=True,
+              required=False,
+              default=True,
+              help='show progress')
+def upload(ctx, catalog_name, file_name, item_name, progress):
+    try:
+        client = ctx.obj['client']
+        org_name = ctx.obj['profiles'].get('org')
+        in_use_org_href = ctx.obj['profiles'].get('org_href')
+        org = Org(client, in_use_org_href, org_name == 'System')
+        cb = callback if progress else None
+        result = org.upload_media(catalog_name,
+                                  file_name,
+                                  item_name,
+                                  callback=cb)
+        stdout(to_dict(result), ctx)
+    except Exception as e:
+        stderr(e, ctx)
+
+
+@catalog.command(short_help='download file from catalog')
+@click.pass_context
+@click.argument('catalog-name',
+                metavar='<catalog-name>')
+def download(ctx, catalog_name):
+    try:
+        raise Exception('not-implemented')
+    except Exception as e:
+        stderr(e, ctx)
+
+
+@catalog.command('list-items', short_help='list catalog items')
+@click.pass_context
+@click.argument('catalog-name',
+                metavar='<catalog-name>')
+def list_items(ctx, catalog_name):
+    try:
+        client = ctx.obj['client']
+        org_name = ctx.obj['profiles'].get('org')
+        in_use_org_href = ctx.obj['profiles'].get('org_href')
+        org = Org(client, in_use_org_href, org_name == 'System')
+        stdout(org.list_catalog_items(catalog_name), ctx, show_id=True)
+    except Exception as e:
+        stderr(e, ctx)
+
+
+@catalog.command('delete-item', short_help='delete a catalog item')
+@click.pass_context
+@click.argument('catalog-name',
+                metavar='<catalog-name>')
+@click.argument('item-name',
+                metavar='<item-name>')
+def delete_item(ctx, catalog_name, item_name):
+    try:
+        client = ctx.obj['client']
+        org_name = ctx.obj['profiles'].get('org')
+        in_use_org_href = ctx.obj['profiles'].get('org_href')
+        org = Org(client, in_use_org_href, org_name == 'System')
+        org.delete_catalog_item(catalog_name, item_name)
+        stdout('Item deleted.', ctx)
     except Exception as e:
         stderr(e, ctx)

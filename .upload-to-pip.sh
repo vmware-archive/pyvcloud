2f68ef5ebcd70226109fc0fbf8d9f957423739d6
Paco Gómez <contact@pacogomez.com>
2017-08-29 16:11:52 -0500

    wip

--- .upload-to-pip.sh
+++ .upload-to-pip.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 if [[ `git describe` == `python setup.py --version` && `git describe` == *-* ]]; then
+  echo 'will skip this publishing this commit, a commit with tag is comming next'
 else
   pip install twine
   rm -rf build dist

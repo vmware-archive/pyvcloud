a9227eec2bdbdcddd5e92ac723f8c99be8e44432
Paco Gómez <contact@pacogomez.com>
2017-08-29 16:29:36 -0500

    wip

--- .upload-to-pip.sh
+++ .upload-to-pip.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+echo "git describe:              `git describe`"
+echo "python setup.py --version: `python setup.py --version`"
 if [[ `git describe` == `python setup.py --version` && `git describe` == *-* ]]; then
   echo 'will skip this publishing this commit, a commit with tag is comming next'
 else

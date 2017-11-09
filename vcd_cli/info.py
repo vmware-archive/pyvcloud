012f7ffe25f8a59e8d18961a1208c9ec2562040c
Paco Gómez <contact@pacogomez.com>
2017-11-09 13:11:39 -0600

    updated info examples

--- vcd_cli/info.py
+++ vcd_cli/info.py
@@ -49,10 +49,14 @@ def info(ctx, resource_type, resource_id):
         vcd info vapp c48a4e1a-7bd9-4177-9c67-4c330016b99f
             Get details of vApp by id.
 \b
-        vcd catalog list my-catalog                                 # list items
-        vcd catalog info my-catalog my-item                         # get catalog item id
-         vcd info catalogitem f653b137-0d14-4ea9-8f14-dcd2b7914110  # get vapp template id
-         vcd info vapptemplate 53b83b27-1f2b-488e-9020-a27aee8cb640 # details of template
+        vcd catalog list my-catalog
+            List items in a catalog.
+        vcd catalog info my-catalog my-item
+            Get details of an item listed in the previous command.
+        vcd info catalogitem f653b137-0d14-4ea9-8f14-dcd2b7914110
+            Get details of the catalog item based on the id listed in the previous command.
+        vcd info vapptemplate 53b83b27-1f2b-488e-9020-a27aee8cb640
+            Get details of the vApp tepmlate based on the id listed int the previous command.
 \b
     See Also
         Several 'vcd' commands provide the id of a resource, including the

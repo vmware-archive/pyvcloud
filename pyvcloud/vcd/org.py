034e8956076808e19f02908e30bdad4e0ef2ad6e
Aritra Sen <aritra.iitkgp@gmail.com>
2017-12-02 22:10:48 -0800

    Refactoring is_admin field in org.py, moving the relevant logic in client.py and fixing corresponding unit tests.

--- pyvcloud/vcd/org.py
+++ pyvcloud/vcd/org.py
@@ -42,15 +42,12 @@ class Org(object):
     def __init__(self,
                  client,
                  href=None,
-                 is_admin=False,
                  resource=None):
         """
         Constructor for Org objects.
 
         :param client: (pyvcloud.vcd.client): The client.
         :param href: (str): URI of the entity.
-        :param is_admin: (bool): Indicates if current logged in user is system administrator.
-            Org doesn't need to be 'System'.
         :param resource: (lxml.objectify.ObjectifiedElement): XML representation of the entity.
 
         """  # NOQA
@@ -61,7 +58,6 @@ class Org(object):
             self.href = resource.get('href')
         self.href_admin = self.href.replace('/api/org/',
                                             '/api/admin/org/')
-        self.is_admin = is_admin
 
     def get_name(self):
         if self.resource is None:
@@ -93,7 +89,7 @@ class Org(object):
         raise Exception('Catalog not found.')
 
     def list_catalogs(self):
-        if self.is_admin:
+        if self.client.is_loggedin_user_sys_admin():
             resource_type = 'adminCatalog'
         else:
             resource_type = 'catalog'
@@ -112,7 +108,7 @@ class Org(object):
     def get_catalog(self, name):
         return self.get_catalog_resource(name, False)
 
-    def get_catalog_resource(self, name, is_admin_operation=False):
+    def get_catalog_resource(self, name, is_admin_operation = False):
         org = self.client.get_resource(self.href)
         links = get_links(org,
                           rel=RelationType.DOWN,
@@ -490,9 +486,11 @@ class Org(object):
         """ # NOQA
         if self.resource is None:
             self.resource = self.client.get_resource(self.href)
-        resource_type = 'adminUser' if self.is_admin else 'user'
-        org_filter = 'org==%s' % self.resource.get('href') \
-            if self.is_admin else None
+        resource_type = 'user'
+        org_filter = None
+        if self.client.is_loggedin_user_sys_admin():
+            resource_type = 'adminUser'
+            org_filter ='org==%s' % self.resource.get('href')
         query = self.client.get_typed_query(
             resource_type,
             query_result_format=QueryResultFormat.REFERENCES,
@@ -553,12 +551,13 @@ class Org(object):
         """  # NOQA
         if self.resource is None:
             self.resource = self.client.get_resource(self.href)
+
         org_filter = None
-        if self.is_admin:
+        resource_type = 'role'
+        if self.client.is_loggedin_user_sys_admin():
             resource_type = 'adminRole'
             org_filter = 'org==%s' % self.resource.get('href')
-        else:
-            resource_type = 'role'
+
         query = self.client.get_typed_query(
             resource_type,
             query_result_format=QueryResultFormat.RECORDS,

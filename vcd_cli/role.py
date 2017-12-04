8bd54b79067b6dc5b483df35d4b5deff8e3d77ef
sahithi <sahithi.ayloo@gmail.com>
2017-12-04 09:45:27 -0800

    [vcd-cli] List rights for a given role in an Org
    - role.py: added a command to list rights for a given role

--- vcd_cli/role.py
+++ vcd_cli/role.py
@@ -1,6 +1,8 @@
 import click
 from pyvcloud.vcd.org import Org
+from pyvcloud.vcd.role import Role
 
+from vcd_cli.utils import is_sysadmin
 from vcd_cli.utils import restore_session
 from vcd_cli.utils import stderr
 from vcd_cli.utils import stdout
@@ -34,5 +36,24 @@ def list_roles(ctx):
         org = Org(client, in_use_org_href, org_name == 'System')
         result = org.list_roles()
         stdout(result, ctx)
+    except Exception as e:
+        stderr(e, ctx)
+
+
+@role.command('list-rights', short_help='list rights of a role')
+@click.pass_context
+@click.argument('role-name',
+                metavar='<role-name>',
+                required=True)
+def list_rights(ctx, role_name):
+    try:
+        client = ctx.obj['client']
+        org_name = ctx.obj['profiles'].get('org')
+        in_use_org_href = ctx.obj['profiles'].get('org_href')
+        org = Org(client, in_use_org_href, is_sysadmin(ctx))
+        role_record = org.get_role(role_name)
+        role = Role(client, href=role_record.get('href'))
+        rights = role.list_rights()
+        stdout(rights, ctx)
     except Exception as e:
         stderr(e, ctx)

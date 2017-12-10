050a6a50f7b1c092b51d8350924e1357487a0e78
sahithi <sahithi.ayloo@gmail.com>
2017-12-10 00:29:44 -0800

    [vcd-cli] list rights in an org

--- vcd_cli/right.py
+++ vcd_cli/right.py
@@ -31,16 +31,14 @@ def right(ctx):
               '--org',
               'org_name',
               required=False,
-              metavar='<org_name>',
+              metavar='<org-name>',
               help='name of the org')
 def list_rights(ctx, org_name):
     try:
         client = ctx.obj['client']
-        if org_name is not None:
-            org_href = client.get_org_by_name(org_name).get('href')
-        else:
-            org_href = ctx.obj['profiles'].get('org_href')
-        org = Org(client, org_href)
+        if org_name is None:
+            org_name = ctx.obj['profiles'].get('org_in_use')
+        org = Org(client, resource=client.get_org_by_name(org_name))
         result = org.list_rights()
         stdout(result, ctx)
     except Exception as e:

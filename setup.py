b5a93f1934eb3036546810d9d707d683a5a56b80
Robert Hodges <rhodges@vmware.com>
2018-02-12 16:30:14 -0800

    VCDA-454: Updated copyrights for release (#204)
    Updated .py file copyright dates to most recent commit and extended back
    in one case to first known commit from git log. Copyright dates follow
    earliest-latest date convention.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 # VMware vCloud Python SDK
-# Copyright (c) 2014 VMware, Inc. All Rights Reserved.
+# Copyright (c) 2014-2017 VMware, Inc. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.

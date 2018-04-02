efd401799c894fa08ac09239c4ccc4af60ad9045
floriankammermann <florian@kammermann.me>
2018-04-02 09:33:48 +0200

    added tests for get_edge_gateway

--- tests/vcd_edge_gateway.py
+++ tests/vcd_edge_gateway.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import unittest
-
 from pyvcloud.vcd.org import Org
 from pyvcloud.vcd.test import TestCase
 from pyvcloud.vcd.vdc import VDC
@@ -30,18 +28,30 @@ class TestEdgeGateway(TestCase):
         edge_gateways = vdc.list_edge_gateways()
         assert len(edge_gateways) > 0
 
-    def test_002_get_nat_rules(self):
+    def test_002_get_edge_gateway(self):
+        logged_in_org = self.client.get_org()
+        org = Org(self.client, resource=logged_in_org)
+        v = org.get_vdc(self.config['vcd']['vdc'])
+        vdc = VDC(self.client, resource=v)
+        edge_gateway = vdc.get_edge_gateway(self.config['vcd']['edge_gateway'])
+        assert edge_gateway.get('name') == self.config['vcd']['edge_gateway']
+
+    def test_003_get_edge_gateway_not_existent(self):
+        logged_in_org = self.client.get_org()
+        org = Org(self.client, resource=logged_in_org)
+        v = org.get_vdc(self.config['vcd']['vdc'])
+        vdc = VDC(self.client, resource=v)
+        with self.assertRaisesRegex(Exception, 'Edge Gateway not found \'' +
+                                    self.config['vcd']['edge_gateway'] +
+                                    '_not_existent\'') as exception:
+            assert exception
+            vdc.get_edge_gateway(self.config['vcd']['edge_gateway'] +
+                                 '_not_existent')
+
+    def test_004_get_nat_rules(self):
         logged_in_org = self.client.get_org()
         org = Org(self.client, resource=logged_in_org)
         v = org.get_vdc(self.config['vcd']['vdc'])
         vdc = VDC(self.client, resource=v)
         nat_rules = vdc.get_nat_rules(self.config['vcd']['edge_gateway'])
         assert len(nat_rules) > 0
-
-    def test_003_get_nat_rules_edge_gateway_not_existent(self):
-        logged_in_org = self.client.get_org()
-        org = Org(self.client, resource=logged_in_org)
-        v = org.get_vdc(self.config['vcd']['vdc'])
-        vdc = VDC(self.client, resource=v)
-        with self.assertRaisesRegex(Exception, 'Edge Gateway not found \''+self.config['vcd']['edge_gateway']+'_not_existent\'') as exception:
-            vdc.get_nat_rules(self.config['vcd']['edge_gateway']+'_not_existent')

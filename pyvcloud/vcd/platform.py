e26c35c9cea88f9cae37af5f0250a38d92b855c8
Curt Kolovson <ckolovson@vmware.com>
2018-04-26 17:47:39 -0700

    [VCDA-605] add optional root_folder parameter to attach VC (#233)
    - add optional root_folder parameter to attach VC

--- pyvcloud/vcd/platform.py
+++ pyvcloud/vcd/platform.py
@@ -98,7 +98,7 @@ class Platform(object):
         raise Exception('External network \'%s\' not found.' % name)
 
     def get_vxlan_network_pool(self, vxlan_network_pool_name):
-        """Fetch a vxlan_networ_pool by its name.
+        """Fetch a vxlan_network_pool by its name.
 
         :param: vxlan_network_pool_name (str): name of the vxlan_network_pool.
         :return: (lxml.objectify.ObjectifiedElement): vxlan_network_pool.
@@ -220,6 +220,7 @@ class Platform(object):
                        vc_server_host,
                        vc_admin_user,
                        vc_admin_pwd,
+                       vc_root_folder=None,
                        nsx_server_name=None,
                        nsx_host=None,
                        nsx_admin_user=None,
@@ -227,10 +228,11 @@ class Platform(object):
                        is_enabled=None):
         """Register (attach) a VirtualCenter server (also known as VimServer).
 
-        :param: vc_server_name: (str): vc_server_name (VC name).
-        :param: vc_server_host: (str): FQDN or IP address of VC host.
-        :param: vc_admin_user: (str): vc_admin user.
-        :param: vc_admin_pwd: (str): vc_admin password.
+        :param: vc_server_name: (str): vc server name (virtual center name).
+        :param: vc_server_host: (str): FQDN or IP address of vc host.
+        :param: vc_admin_user: (str): vc admin user.
+        :param: vc_admin_pwd: (str): vc admin password.
+        :param: vc_root_folder: (str): vc root folder.
         :param: nsx_server_name: (str): NSX server name.
         :param: nsx_host (str): FQDN or IP address of NSX host.
         :param: nsx_admin_user: (str): NSX admin user.
@@ -245,6 +247,8 @@ class Platform(object):
         vc_server.append(E_VMEXT.Url('https://' + vc_server_host + ':443'))
         if is_enabled is not None:
             vc_server.append(E_VMEXT.IsEnabled(is_enabled))
+        if vc_root_folder is not None:
+            vc_server.append(E_VMEXT.rootFolder(vc_root_folder))
         register_vc_server_params.append(vc_server)
         if nsx_server_name is not None:
             nsx_manager = E_VMEXT.ShieldManager(name=nsx_server_name)

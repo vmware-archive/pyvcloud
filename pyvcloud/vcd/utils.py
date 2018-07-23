3cc26c5ce7ddf7fc4cd57cb7e99f880cebc6b414
rocknes <aritra.iitkgp@gmail.com>
2018-07-23 16:29:42 -0700

    Addressing review comments.

--- pyvcloud/vcd/utils.py
+++ pyvcloud/vcd/utils.py
@@ -693,9 +693,9 @@ def get_safe_members_in_tar_file(tarfile):
         if _badpath(finfo.name, base):
             print(finfo.name + ' is blocked: illegal path.')
         elif finfo.issym() and _badlink(finfo, base):
-            print(finfo.name + ' is blocked: Hard link to ' + finfo.linkname)
-        elif finfo.islnk() and _badlink(finfo, base):
             print(finfo.name + ' is blocked: Symlink to ' + finfo.linkname)
+        elif finfo.islnk() and _badlink(finfo, base):
+            print(finfo.name + ' is blocked: Hard link to ' + finfo.linkname)
         else:
             result.append(finfo)
     return result

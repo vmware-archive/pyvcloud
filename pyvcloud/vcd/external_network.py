bbc3da412457b6398a30213657983cdcc1f21eec
SAURABH PANDEY <pandeys@vmware.com>
2019-01-03 15:14:11 +0530

    VP 1447: [PySDK] Delete an IP range of a subnet in external network (#349)
    * VP 1447: [PySDK] Delete an IP range of a subnet in external network
    
    [PySDK] Delete an IP range of a subnet in external network
    
    This changelist contains deletion of an IP range from a gateway of an
    external network. This CLN also has test case related to this
    functionality.
    
    Testing done:
    Test case for delete an ip range  is added to a test class
    extnet_tests.py
     All tests in extnet_tests passed.
    
    * VP 1447: [PySDK] Delete an IP range of a subnet in external network
    
    [PySDK] Delete an IP range of a subnet in external network
    
     This changelist contains deletion of an IP range from a gateway of an
    external network. This CLN also has test case related to this
    functionality.
    
    Testing done:
    Test case for delete an ip range  is added to a test class
    extnet_tests.py
    All tests in extnet_tests passed.

--- pyvcloud/vcd/external_network.py
+++ pyvcloud/vcd/external_network.py
@@ -245,6 +245,51 @@ class ExternalNetwork(object):
                                 EXTERNAL_NETWORK.value,
                                 contents=ext_net)
 
+    def delete_ip_range(self, gateway_ip, ip_range):
+        """Delete ip range of a subnet in external network.
+
+        :param str gateway_ip: IP address of the gateway of external
+             network.
+
+        :param str ip_range: existing ip range present in the static pool
+             allocation in the network to be deleted.
+             For example, [192.168.1.2-192.168.1.20]
+
+        :return: object containing vmext:VMWExternalNetwork XML element that
+             representing the external network.
+
+        :rtype: lxml.objectify.ObjectifiedElement
+        """
+        if self.resource is None:
+            self.reload()
+        ext_net = self.resource
+        ip_addrs = ip_range.split('-')
+        config = ext_net['{' + NSMAP['vcloud'] + '}Configuration']
+        ip_scopes = config.IpScopes
+        ip_range_found = False
+
+        for ip_scope in ip_scopes.IpScope:
+            if ip_scope.Gateway == gateway_ip:
+                ip_ranges = ip_scope.IpRanges
+                for exist_ip_range in ip_scope.IpRanges.IpRange:
+                    if exist_ip_range.StartAddress == \
+                            ip_addrs[0] and \
+                            exist_ip_range.EndAddress \
+                            == ip_addrs[1]:
+                        ip_ranges.remove(exist_ip_range)
+                        ip_range_found = True
+                        break
+
+        if not ip_range_found:
+            raise EntityNotFoundException(
+                'IP Range \'%s\' not Found' % ip_range)
+
+        return self.client. \
+            put_linked_resource(ext_net, rel=RelationType.EDIT,
+                                media_type=EntityType.
+                                EXTERNAL_NETWORK.value,
+                                contents=ext_net)
+
     def attach_port_group(self, vim_server_name, port_group_name):
         """Attach a portgroup to an external network.
 

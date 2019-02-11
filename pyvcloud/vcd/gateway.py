71145fe25e43c768f9463e32318e27490a61321e
Ankit Gupta <guptaankit@vmware.com>
2019-02-11 11:14:19 +0530

    Fix for enable/disable HA

--- pyvcloud/vcd/gateway.py
+++ pyvcloud/vcd/gateway.py
@@ -367,33 +367,31 @@ class Gateway(object):
     def edit_gateway(self, newname=None, desc=None, ha=None):
         """It changes the old name of the gateway to the new name.
 
-        :param Str newname: new name of the gateway
-        :param Str desc: description of the gateway
+        :param str newname: new name of the gateway
+        :param str desc: description of the gateway
         :param bool ha: HA of the gateway
 
         :return: object containing EntityType.TASK XML data
             representing the asynchronous task.
 
         :rtype: lxml.objectify.ObjectifiedElement
+
+        :raises: ValueError: if the values are not provided.
         """
-        non_empty_param = True
+        if newname is None and desc is None and ha is None:
+            raise ValueError('Invalid input, please provide at least one '
+                             'parameter')
 
         gateway = self.get_resource()
         if newname:
-            non_empty_param = False
             gateway.set('name', newname)
         if desc:
-            non_empty_param = False
             if hasattr(gateway, 'Description'):
                 gateway.Description = E.Description(desc)
             else:
                 gateway.insert(0, E.Description(desc))
         if ha:
-            non_empty_param = False
             gateway.Configuration.HaEnabled = E.HaEnabled(ha)
-
-        if non_empty_param is True:
-            raise ValueError('Invalid input,please provide any one parameter.')
 
         return self.client.put_linked_resource(
             self.resource, RelationType.EDIT, EntityType.EDGE_GATEWAY.value,

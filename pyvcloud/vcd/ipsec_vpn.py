e8d3e8275d52ff9b43d56a58daa02b4ba7b3f523
SAURABH PANDEY <pandeys@vmware.com>
2019-02-20 12:08:26 +0530

    VP-1723,VP-1775: Update Ipsec Vpn, Info VPN site (#455)
    * VP-1723,VP-1775: Update Ipsec Vpn, Info VPN site
    
    VP-1723: [PySDK ] Edit IPsec VPN Sites
    VP-1775: [PYSDK] Info VPN site
    
    This CLN contains both functionalities and correspoding test cases.
    
    Testing Done:
    All test cases in ipsec_vpn_test class are passing.
    
    * VP-1723,VP-1775: Update Ipsec Vpn, Info VPN site
    
    VP-1723: [PySDK ] Edit IPsec VPN Sites
    VP-1775: [PYSDK] Info VPN site
    
    This CLN contains both functionalities and correspoding test cases.
    
    Testing Done:
    All test cases in ipsec_vpn_test class are passing.
    
    * VP-1723,VP-1775: Update Ipsec Vpn, Info VPN site
    
    VP-1723: [PySDK ] Edit IPsec VPN Sites
    VP-1775: [PYSDK] Info VPN site
    
    This CLN contains both functionalities and correspoding test cases.
    
    Testing Done:
    All test cases in ipsec_vpn_test class are passing.
    *Incorporated review comments.
    
    * VP-1723,VP-1775: Update Ipsec Vpn, Info VPN site
    
    VP-1723: [PySDK ] Edit IPsec VPN Sites
    VP-1775: [PYSDK] Info VPN site
    
    This CLN contains both functionalities and correspoding test cases.
    
    Testing Done:
    All test cases in ipsec_vpn_test class are passing.

--- pyvcloud/vcd/ipsec_vpn.py
+++ pyvcloud/vcd/ipsec_vpn.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from pyvcloud.vcd.client import E
 from pyvcloud.vcd.client import EntityType
 from pyvcloud.vcd.gateway_services import GatewayServices
 from pyvcloud.vcd.network_url_constants import IPSEC_VPN_URL_TEMPLATE
@@ -56,7 +57,142 @@ class IpsecVpn(GatewayServices):
         for site in vpn_sites.site:
             if site.localIp == local_ip and site.peerIp == peer_ip:
                 vpn_sites.remove(site)
+                break
 
         self.client.put_resource(self.href,
                                  ipsec_vpn,
                                  EntityType.DEFAULT_CONTENT_TYPE.value)
+
+    def update_ipsec_vpn(self,
+                         name=None,
+                         peer_id=None,
+                         peer_ip_address=None,
+                         local_id=None,
+                         local_ip_address=None,
+                         local_subnet=None,
+                         peer_subnet=None,
+                         shared_secret_encrypted=None,
+                         encryption_protocol=None,
+                         authentication_mode=None,
+                         dh_group=None,
+                         description=None,
+                         mtu=None,
+                         is_enabled=None,
+                         enable_pfs=None):
+        """Update IPsec VPN of the gateway.
+
+        param str name: new name of IPSec VPN
+        param str description: new description of IPSec VPN
+        param str peer_id: new peer id
+        param str peer_ip_address: new peer IP address
+        param str local_id: new local id
+        param str local_ip_address: new local IP address
+        param str local_subnet: new local subnet in CIDR format
+        param str peer_subnet: new peer subnet in CIDR format
+        param str shared_secret_encrypted: new shared secret encrypted
+        param str encryption_protocol: new encryption protocol
+        param str authentication_mode: new authentication mode
+        param str dh_group: new dh group
+        param str mtu: new MTU
+        param bool is_enabled: new enabled status Default : false
+        param bool enable_pfs: new enabled pfs status Default : false
+        :return: Ipsec Vpn object
+        :rtype: lxml.objectify.ObjectifiedElement
+        """
+        end_points = self.end_point.split('-')
+        local_ip = end_points[0]
+        peer_ip = end_points[1]
+        ipsec_vpn = self.resource
+        vpn_sites = ipsec_vpn.sites
+        for site in vpn_sites.site:
+            if site.localIp == local_ip and site.peerIp == peer_ip:
+                if is_enabled is not None:
+                    site.enabled = E.enabled(is_enabled)
+                if name is not None:
+                    site.name = E.name(name)
+                if description is not None:
+                    site.description = E.description(description)
+                if local_id is not None:
+                    site.localId = E.localId(local_id)
+                if local_ip_address is not None:
+                    site.localIp = E.localIp(local_ip_address)
+                if peer_id is not None:
+                    site.peerId = E.peerId(peer_id)
+                if peer_ip_address is not None:
+                    site.peerIp = E.peerIp(peer_ip_address)
+                if encryption_protocol is not None:
+                    site.encryptionAlgorithm = \
+                        E.encryptionAlgorithm(encryption_protocol)
+                if mtu is not None:
+                    site.mtu = E.mtu(mtu)
+                if enable_pfs is not None:
+                    site.enablePfs = E.enablePfs(enable_pfs)
+                if local_subnet is not None:
+                    local_subnets = E.localSubnets()
+                    if ',' in local_subnet:
+                        subnet_list = local_subnet.split(",")
+                        for subnet in subnet_list:
+                            local_subnets.append(E.subnet(subnet))
+                    else:
+                        local_subnets.append(E.subnet(local_subnet))
+                    site.localSubnets = local_subnets
+                if peer_subnet is not None:
+                    peer_subnets = E.peerSubnets()
+                    if ',' in peer_subnet:
+                        subnet_list = peer_subnet.split(",")
+                        for subnet in subnet_list:
+                            peer_subnets.append(E.subnet(subnet))
+                    else:
+                        peer_subnets.append(E.subnet(peer_subnet))
+                    site.peerSubnets = peer_subnets
+                if shared_secret_encrypted is not None:
+                    site.psk = E.psk(shared_secret_encrypted)
+                if authentication_mode is not None:
+                    site.authenticationMode = \
+                        E.authenticationMode(authentication_mode)
+                if dh_group is not None:
+                    site.dhGroup = E.dhGroup(dh_group)
+                break
+
+        self.client.put_resource(self.href, ipsec_vpn,
+                                 EntityType.DEFAULT_CONTENT_TYPE.value)
+
+    def get_vpn_site_info(self):
+        """Get the details of Ipsec VPN site.
+
+        :return: Dictionary having nat rule details.
+
+        :rtype: Dictionary
+        """
+        vpn_site_info = {}
+        end_points = self.end_point.split('-')
+        local_ip = end_points[0]
+        peer_ip = end_points[1]
+        ipsec_vpn = self.resource
+        vpn_sites = ipsec_vpn.sites
+        for site in vpn_sites.site:
+            if site.localIp == local_ip and site.peerIp == peer_ip:
+                vpn_site_info['Name'] = site.name
+                vpn_site_info['description'] = site.description
+                vpn_site_info['localId'] = site.localId
+                vpn_site_info['localIp'] = site.localIp
+                vpn_site_info['peerId'] = site.peerId
+                vpn_site_info['peerIp'] = site.peerIp
+                localsubnets = ''
+                for subnet in site.localSubnets.subnet:
+                    localsubnets = localsubnets + subnet + '-'
+                localsubnets = localsubnets[:-1]
+                vpn_site_info['localSubnets'] = localsubnets
+                peersubnets = ''
+                for subnet in site.peerSubnets.subnet:
+                    peersubnets = peersubnets + subnet + '-'
+                peersubnets = peersubnets[:-1]
+                vpn_site_info['peerSubnets'] = peersubnets
+                vpn_site_info['enabled'] = site.enabled
+                vpn_site_info['encryptionAlgorithm'] = site.encryptionAlgorithm
+                vpn_site_info['mtu'] = site.mtu
+                vpn_site_info['enablePfs'] = site.enablePfs
+                vpn_site_info['dhGroup'] = site.dhGroup
+                break
+
+        return vpn_site_info

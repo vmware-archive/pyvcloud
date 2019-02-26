7254afea8116b13f6c786053f01f05189bbfc30d
SAURABH PANDEY <pandeys@vmware.com>
2019-02-26 13:59:00 +0530

    VP-1910:[PYSDK] Add service certificate
    [PYSDK] Add service certificate
    
    This CLN contains above functionality and corresponding test case.
    
    Testing Done: Executed the certificate_tests successfully.

--- pyvcloud/vcd/network_url_constants.py
+++ pyvcloud/vcd/network_url_constants.py
@@ -29,3 +29,5 @@ NAT_RULES_URL_TEMPLATE = "/nat/config" + NAT_RULES
 NAT_RULE_URL_TEMPLATE = NAT_RULES_URL_TEMPLATE + "/{0}"
 STATIC_ROUTE_URL_TEMPLATE = "/routing/config/static"
 IPSEC_VPN_URL_TEMPLATE = "/ipsec/config"
+SERVICE_CERTIFICATE_POST = "/services/truststore/certificate/"
+GET_CERTIFICATES = "/services/truststore/certificate/scope/"

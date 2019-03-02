8105af3dab4782daf2cbf04fdfc954d2429edf85
Vladimir Chitashvili <vladimir.chitashvili@telia.ee>
2019-03-02 02:11:14 +0200

    changing order of imports in alphabetic order

--- pyvcloud/vcd/org.py
+++ pyvcloud/vcd/org.py
@@ -32,11 +32,11 @@ from pyvcloud.vcd.client import E_OVF
 from pyvcloud.vcd.client import EntityType
 from pyvcloud.vcd.client import find_link
 from pyvcloud.vcd.client import get_links
-from pyvcloud.vcd.client import NSMAP
-from pyvcloud.vcd.client import QueryResultFormat
 from pyvcloud.vcd.client import MetadataDomain
 from pyvcloud.vcd.client import MetadataValueType
 from pyvcloud.vcd.client import MetadataVisibility
+from pyvcloud.vcd.client import NSMAP
+from pyvcloud.vcd.client import QueryResultFormat
 from pyvcloud.vcd.client import RelationType
 from pyvcloud.vcd.client import ResourceType
 from pyvcloud.vcd.exceptions import DownloadException

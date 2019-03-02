6fb66bd378429bfb2ef96455ef32c2c0a9ebd8d1
Vladimir Chitashvili <vladimir.chitashvili@telia.ee>
2019-03-02 02:04:44 +0200

    changing order of import based on flake8

--- pyvcloud/vcd/org.py
+++ pyvcloud/vcd/org.py
@@ -36,9 +36,9 @@ from pyvcloud.vcd.client import NSMAP
 from pyvcloud.vcd.client import QueryResultFormat
 from pyvcloud.vcd.client import RelationType
 from pyvcloud.vcd.client import MetadataDomain
-from pyvcloud.vcd.client import ResourceType
 from pyvcloud.vcd.client import MetadataValueType
 from pyvcloud.vcd.client import MetadataVisibility
+from pyvcloud.vcd.client import ResourceType
 from pyvcloud.vcd.exceptions import DownloadException
 from pyvcloud.vcd.exceptions import EntityNotFoundException
 from pyvcloud.vcd.exceptions import InvalidParameterException

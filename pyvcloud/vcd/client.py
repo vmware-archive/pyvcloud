d943cb94dd33264049456287ac3a3b3c51469dc7
Kishan Kothari <kotharik@vmware.com>
2019-03-20 10:41:28 +0530

    Logger Change

--- pyvcloud/vcd/client.py
+++ pyvcloud/vcd/client.py
@@ -1750,6 +1750,30 @@ def get_logger(file_name="vcd_sdk.log",
                log_level=logging.DEBUG,
                max_bytes=10000000,
                backup_count=10):
+    """This will give a logger with Rotating FileHandler.
+
+    Open the specified file and use it as the stream for logging.
+    By default, the file grows indefinitely. You can specify particular
+    values of maxBytes and backupCount to allow the file to rollover at
+    a predetermined size.
+    Rollover occurs whenever the current log file is nearly maxBytes in
+    length. If backupCount is >= 1, the system will successively create
+    new files with the same pathname as the base file, but with extensions
+    ".1", ".2" etc. appended to it. For example, with a backupCount of 5
+    and a base file name of "app.log", you would get "app.log",
+    "app.log.1", "app.log.2", ... through to "app.log.5". The file being
+    written to is always "app.log" - when it gets filled up, it is closed
+    and renamed to "app.log.1", and if files "app.log.1", "app.log.2" etc.
+    exist, then they are renamed to "app.log.2", "app.log.3" etc.
+    respectively.
+
+    :param file_name: name of the log file.
+    :param log_level: log level.
+    :param max_bytes: max size of log file in bytes.
+    :param backup_count: no of backup count.
+    :return: Logger with rotating file handler.
+    :type: LOGGER
+    """
     LOGGER = logging.getLogger(file_name)
     logHandler = handlers.RotatingFileHandler(
         filename=file_name, maxBytes=max_bytes, backupCount=backup_count)

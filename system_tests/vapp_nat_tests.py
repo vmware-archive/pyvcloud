96c67b6985163a7d2aa473d47e1c876bfd12c001
Kishan Kothari <kotharik@vmware.com>
2019-08-06 11:01:10 +0530

    VP-2746

--- system_tests/vapp_nat_tests.py
+++ system_tests/vapp_nat_tests.py
@@ -38,6 +38,9 @@ class TestVappNat(BaseTestCase):
     _policy_traffic = 'allowTraffic'
     _policy_traffic_in = 'allowTrafficIn'
     _allocate_ip_address = '90.80.70.10'
+    _rule_id = '65537'
+    _ext_ip_addr = '2.2.3.20'
+    _manual = 'manual'
 
     def test_0000_setup(self):
         TestVappNat._client = Environment.get_sys_admin_client()
@@ -141,13 +144,29 @@ class TestVappNat(BaseTestCase):
         self.assertEqual(result[0]['VAppScopedVmId'], TestVappNat._vm_id)
         self.assertEqual(str(result[0]['VmNicId']), TestVappNat._vm_nic_id)
 
+    def test_0060_update_nat_rule(self):
+        vapp_nat = VappNat(TestVappNat._client,
+                           vapp_name=TestVappNat._vapp_name,
+                           network_name=TestVappNat._network_name)
+        task = vapp_nat.update_nat_rule(
+            rule_id=TestVappNat._rule_id,
+            mapping_mode=TestVappNat._manual,
+            external_ip_address=TestVappNat._ext_ip_addr)
+        result = TestVappNat._client.get_task_monitor().wait_for_success(task)
+        self.assertEqual(result.get('status'), TaskStatus.SUCCESS.value)
+        vapp_nat._reload()
+        nat_service = vapp_nat.resource.Configuration.Features.NatService
+        one_to_one_rule = nat_service.NatRule.OneToOneVmRule
+        self.assertEqual(one_to_one_rule.MappingMode, TestVappNat._manual)
+        self.assertEqual(one_to_one_rule.ExternalIpAddress,
+                         TestVappNat._ext_ip_addr)
+
     def test_0090_delete_nat_rule(self):
         vapp_nat = VappNat(TestVappNat._client,
                            vapp_name=TestVappNat._vapp_name,
                            network_name=TestVappNat._network_name)
         vapp_nat._get_resource()
-        id = vapp_nat.resource.Configuration.Features.NatService.NatRule.Id
-        task = vapp_nat.delete_nat_rule(id)
+        task = vapp_nat.delete_nat_rule(TestVappNat._rule_id)
         result = TestVappNat._client.get_task_monitor().wait_for_success(task)
         self.assertEqual(result.get('status'), TaskStatus.SUCCESS.value)
         vapp_nat._reload()
